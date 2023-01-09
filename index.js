56a60d92b363148fa7ed81a82bc3e44d23209871
vishalicious213 <vishalicious213@gmail.com>
2023-01-08 19:06:56 -0800

    updated handleUpdate() to grab each input by its name and update its value (and append the value suffix, like 'px')

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inputs = document.querySelectorAll(".controls input")
 
 function handleUpdate() {
-    console.log(this.value)
+    const suffix = this.dataset.sizing || ""
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+    // console.log(this.name, suffix)
 }
 
 // run handleUpdate on each input. trigger when there's a value change or when the mouse moves over the element
