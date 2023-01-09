be0e22f395232685826f06135e916d9e063330cb
vishalicious213 <vishalicious213@gmail.com>
2023-01-08 18:30:34 -0800

    grabbed all of the inputs on the screen and assigned an event listener to each that triggers on change

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("CSS Variables")
+const inputs = document.querySelectorAll(".controls input")
+
+function handleUpdate() {
+    console.log(this.value)
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate))
