f1d19be176a5d9fd6ec48d8d2a98edf923976cd9
vishalicious213 <vishalicious213@gmail.com>
2023-01-08 18:33:37 -0800

    assigned a second event listener to each that triggers when the mouse moves over the input

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function handleUpdate() {
     console.log(this.value)
 }
 
+// run handleUpdate on each input. trigger when there's a value change or when the mouse moves over the element
 inputs.forEach(input => input.addEventListener("change", handleUpdate))
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate))
